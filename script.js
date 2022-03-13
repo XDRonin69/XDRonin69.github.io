2fdff16b25bd5ccefff596c8b8cf452e2830fffa
XDRonin69 <91185648+XDRonin69@users.noreply.github.com>
2022-03-13 21:27:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -201,9 +201,3 @@ function readMoreFunction(n) {
 
 }
 
-document.querySelectorAll('*').forEach(el => {
-  if (el.offsetWidth > document.documentElement.offsetWidth) {
-      console.log('Found the worst element ever: ', el);
-  }
-});
-console.log('tested all');
